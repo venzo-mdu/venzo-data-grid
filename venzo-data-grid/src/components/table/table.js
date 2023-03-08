06855793390d1e4438cc817ad1aeb2d212751048
Gowthama-V <gowthama@venzotechnologies.com>
2023-03-08 15:00:46 +0530

    short

--- venzo-data-grid/src/components/table/table.js
+++ venzo-data-grid/src/components/table/table.js
@@ -1,100 +1,124 @@
 import React, { useEffect, useState } from 'react'
 import '../table/table.css'
-import {columnJson ,rowJson} from '../../context/tableData'
+import { columnJson, rowJson } from '../../context/tableData'
 
 function Table() {
-  console.log("json of the table",columnJson,rowJson)
-  const [columnData,setColumnData] = useState([]);
-  const [rowData,setRowData] = useState([]);
+  // console.log("json of the table",columnJson,rowJson)
+  const [columnData, setColumnData] = useState([]);
+  const [rowData, setRowData] = useState([]);
+  const [shorting, setSorting] = useState([])
+  const [order, setOrder] = useState('asd')
 
+  const sortTable = (value, key) => {
+    if (key === "true"){
+      if (value === 'asd') {
+        setSorting([...shorting].sort((a, b) =>
+          a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1
+        ))
+        setOrder('dsc')
+      } else {
+        setSorting([...shorting].sort((a, b) =>
+          a.name.toLowerCase() > b.name.toLowerCase() ? -1 : 1
+        ))
+        setOrder('asd')
+      }
+    }
+   else{
+    setOrder(rowData)
+   }
+    
+  }
 
-
-  useEffect(()=>{
+  console.log(shorting)
+  useEffect(() => {
     setColumnData(columnJson)
     setRowData(rowJson)
-  },[])
- 
+    setSorting(rowJson)
+  }, [])
+
   return (
     <>
-    <div className='tableGrid'>
-      <div className='columnGrid'>
+      <div className='tableGrid'>
+        <div className='columnGrid'>
 
-      {
-        columnData.map(item=>{
-          return(
-            <p style ={{width:item.width,
-              fontFamily:item.fontFamily,
-              fontWeight:item.fontWeight,
-              fontSize:item.fontSize}} 
-              className={item.className}>{item.title}
-              </p>
-          ) 
-        })
-      }
-      </div>
-     <div className='rowGrid'>
+          {
+            columnData.map(item => {
+              return (
+                <p style={{
+                  width: item.width,
+                  fontFamily: item.fontFamily,
+                  fontWeight: item.fontWeight,
+                  fontSize: item.fontSize
+                }}
+                  className={item.className} onClick={() => sortTable(order,item.sortable)}>{item.title}
+                </p>
+              )
+            })
+          }
+        </div>
+        <div className='rowGrid'>
 
-      <div className='names'>
-      {
-        rowData.map(data=>{
-          return(
-            <>
-            <p>{data.name}</p>
-            </>
-          )
-        })
-      }
+          <div className='names'>
+            {
+              shorting.map(data => {
+                return (
+                  <>
+                    <p>{data.name}</p>
+                  </>
+                )
+              })
+            }
+          </div>
+          <div className='country'>
+            {
+              shorting.map(data => {
+                return (
+                  <>
+                    <p>{data.country}</p>
+                  </>
+                )
+              })
+            }
+          </div>
+          <div className='language'>
+            {
+              shorting.map(data => {
+                return (
+                  <>
+                    <p>{data.language}</p>
+                  </>
+                )
+              })
+            }
+          </div>
+          <div className='game'>
+            {
+              shorting.map(data => {
+                return (
+                  <>
+                    <p>{data.game}</p>
+                  </>
+                )
+              })
+            }
+          </div>
+          <div className='medal'>
+            {
+              shorting.map(data => {
+                return (
+                  <>
+                    <p>{data.medal}</p>
+                  </>
+                )
+              })
+            }
+          </div>
+        </div>
+
       </div>
-      <div className='country'>
-      {
-        rowData.map(data=>{
-          return(
-            <>
-            <p>{data.country}</p>
-            </>
-          )
-        })
-      }
-      </div>
-      <div className='language'>
-      {
-        rowData.map(data=>{
-          return(
-            <>
-            <p>{data.language}</p>
-            </>
-          )
-        })
-      }
-      </div>
-      <div className='game'>
-      {
-        rowData.map(data=>{
-          return(
-            <>
-            <p>{data.game}</p>
-            </>
-          )
-        })
-      }
-      </div>
-      <div className='medal'>
-      {
-        rowData.map(data=>{
-          return(
-            <>
-            <p>{data.medal}</p>
-            </>
-          )
-        })
-      }
-      </div>
-     </div>
-   
-    </div>
 
     </>
   )
 }
 
-export default Table
+export default Table;
