07d836b11f1bdc2b5b8006b9f156fe75b5ccb7d5
Gowthama-V <gowthama@venzotechnologies.com>
2023-03-14 11:19:33 +0530

    checkbox

--- venzo-data-grid/src/components/table/table.js
+++ venzo-data-grid/src/components/table/table.js
@@ -134,168 +134,161 @@ import { tableJson } from '../../context/wholetable'
 import Flag from 'react-world-flags';
 
 function Table() {
-const [columnData, setColumnData] = useState([]);
-const [rowData, setRowData] = useState([]);
-const [table, setTable] = useState([]);
-const [sorting, setSorting] = useState([]);
-const [value, setValue] = useState('');
-const [filteredData, setFilteredData] = useState([]);
+  const [columnData, setColumnData] = useState([]);
+  const [rowData, setRowData] = useState([]);
+  const [table, setTable] = useState([]);
+  const [sorting, setSorting] = useState([]);
+  const [value, setValue] = useState('');
+  const [filteredData, setFilteredData] = useState([]);
 
-const handleChange = (e) => {
-setValue(e.target.value);
-};
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
 
-const filterOptionSelected = (e) => {
-let data = e.target.value
-if (data !== null) {
-let filterOption = value
-let filteredData = sorting.filter(element => element[filterOption] === data);
-setFilteredData(filteredData)
-}
-else if (data === ' ') {
-let filteredData = sorting.map(element => element)
-setFilteredData(filteredData)
-}
-}
+  const filterOptionSelected = (e) => {
+    let data = e.target.value
+    if (data !== null) {
+      let filterOption = value
+      let filteredData = sorting.filter(element => element[filterOption] === data);
+      setFilteredData(filteredData)
+    }
+    else if (data === ' ') {
+      let filteredData = sorting.map(element => element)
+      setFilteredData(filteredData)
+    }
+  }
 
-const renderFilterOptions = (filterOption) => {
-if (filterOption !== null) {
-let options = Array.from(new Set(sorting.map(element => element[filterOption])));
-return (
-options.map((option, i) => <option key={i} value={option}> {option} </option>)
-)
-}
-else if (filterOption === ' ') {
-let options = Array.from(new Set(sorting.map(element => element)));
-return options
-}
-}
+  const renderFilterOptions = (filterOption) => {
+    if (filterOption !== null) {
+      let options = Array.from(new Set(sorting.map(element => element[filterOption])));
+      return (
+        options.map((option, i) => <option key={i} value={option}> {option} </option>)
+      )
+    }
+    else if (filterOption === ' ') {
+      let options = Array.from(new Set(sorting.map(element => element)));
+      return options
+    }
+  }
 
-const renderTableData = (data) => {
-return data.map((element, i) => {
-return (
-<div key={i} className="columnGrid" >
+  const renderTableData = (data) => {
+    return data.map((element, i) => {
+      return (
+        <div key={i} name='colordata' className="columnGrid" >
 
-<input key={i} type="checkbox" name='checkbox' onChange={(e) => toggle(e, i)} className='selectRow' />
-{columnData.map((col, index) => {
-return (
+          <input key={i} type="checkbox" name='checkbox' onChange={(e) => toggle(e, i)} className='selectRow' />
+          {columnData.map((col, index) => {
+            return (
 
-<div style={{ paddingTop: '20px', paddingLeft: '10px' }} key={index}>{col.key === 'country' ? <Flag code={element.flag} height='16' /> : ''} &nbsp;{element[col.key]}</div>
-)
-})}
-</div>
-)
-})
-}
+              <div style={{ paddingTop: '20px', paddingLeft: '10px' }} key={index}>{col.key === 'country' ? <Flag code={element.flag} height='16' /> : ''} &nbsp;{element[col.key]}</div>
+            )
+          })}
+        </div>
+      )
+    })
+  }
 
-const [order, setOrder] = useState('asd');
+  const [order, setOrder] = useState('asd');
 
-const sortTable = (value, key) => {
-if (key === "true") {
-if (value === 'asd') {
-setSorting([...sorting].sort((a, b) =>
-a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1
-))
-setOrder('dsc')
-} else {
-setSorting([...sorting].sort((a, b) =>
-a.name.toLowerCase() > b.name.toLowerCase() ? -1 : 1
-))
-setOrder('asd')
-}
-}
-else {
-setSorting(rowData)
-}
+  const sortTable = (value, key) => {
+    if (key === "true") {
+      if (value === 'asd') {
+        setSorting([...sorting].sort((a, b) =>
+          a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1
+        ))
+        setOrder('dsc')
+      } else {
+        setSorting([...sorting].sort((a, b) =>
+          a.name.toLowerCase() > b.name.toLowerCase() ? -1 : 1
+        ))
+        setOrder('asd')
+      }
+    }
+    else {
+      setSorting(rowData)
+    }
 
-}
+  }
 
-const toggle = (event, value) => {
-console.log(sorting[value], event.target)
-let parent = event.target.parentNode
-if (event.target.checked) {
-parent.style.backgroundColor = "#24a0ed"
-} else {
-parent.style.backgroundColor = 'white'
-}
+  const toggle = (event, value) => {
+    console.log(sorting[value], event.target)
+    let parent = event.target.parentNode
+    if (event.target.checked) {
+      parent.style.backgroundColor = "#24a0ed"
+    } else {
+      parent.style.backgroundColor = 'white'
+    }
 
-}
-function selects(e) {
+  }
+  function selects(e) {
+    let data = document.getElementsByName('colordata');
+    var ele = document.getElementsByName('checkbox');
+    for (var i = 0; i < ele.length; i++) {
+      if (e.target.checked) {
+        ele[i].checked = true;
+        data[i].style.backgroundColor ='blue'
+      } else {
+        ele[i].checked = false;
+        data[i].style.backgroundColor ='white'
+      }
+    }
+  }
 
-console.log(e)
-let data = document.getElementsByClassName('tableData')[0]
-console.log(data)
-let checkData = data.getElementsByTagName('input')
-console.log(checkData.length)
-var ele = document.getElementsByName('checkbox');
-for (var i = 0; i < ele.length; i++) {
-if (e.target.checked) {
-ele[i].checked = true;
-data.style.backgroundColor = '#24a0ed'
+  useEffect(() => {
+    setColumnData(columnJson)
+    setRowData(rowJson)
+    setSorting(rowJson)
+    setTable(tableJson)
+  }, [])
 
-} else {
-ele[i].checked = false;
-data.style.backgroundColor = 'white'
+  return (
+    <>
+      <div className='tableGrid' style={{
+        width: table.width,
+        boxShadow: table.boxShadow,
+        margin: table.margin,
+        padding: table.padding,
+        backgroundColor: table.backgroundColor,
+        border: table.border
+      }}>
+        <div>
+          <label>filter</label>&nbsp;&nbsp;&nbsp;
+          <select value={value} onChange={handleChange}>
+            <option value=" "> </option>
+            {columnData.map((item) => {
+              return <option value={item.key}>{item.title}</option>
+            })}
+          </select>
+          <select onChange={filterOptionSelected}>
+            <option value=" "> </option>
+            {renderFilterOptions(value)}
+          </select>
+          <div className='columnGrid'>
+            <input className='selectAll' onClick={(e) => selects(e)} type="checkbox" name="Check_ctr" value="yes" />
+            {columnData.map((item, index) => {
+              return (
+                <div key={index} style={{
+                  width: item.width,
+                  fontFamily: item.fontFamily,
+                  fontWeight: item.fontWeight,
+                  fontSize: item.fontSize
+                }}
+                  className={item.className} onClick={() => sortTable(order, item.sortable)}> {item.title}
+                </div>
+              )
+            })
+            }
+          </div>
+        </div>
 
-}
-}
-}
-
-useEffect(() => {
-setColumnData(columnJson)
-setRowData(rowJson)
-setSorting(rowJson)
-setTable(tableJson)
-}, [])
-
-return (
-<>
-<div className='tableGrid' style={{
-width: table.width,
-boxShadow: table.boxShadow,
-margin: table.margin,
-padding: table.padding,
-backgroundColor: table.backgroundColor,
-border: table.border
-}}>
-<div>
-<label>filter</label>&nbsp;&nbsp;&nbsp;
-<select value={value} onChange={handleChange}>
-<option value=" "> </option>
-{columnData.map((item) => {
-return <option value={item.key}>{item.title}</option>
-})}
-</select>
-<select onChange={filterOptionSelected}>
-<option value=" "> </option>
-{renderFilterOptions(value)}
-</select>
-<div className='columnGrid'>
-<input className='selectAll' onClick={(e) => selects(e)} type="checkbox" name="Check_ctr" value="yes" />
-{columnData.map((item, index) => {
-return (
-<div key={index} style={{
-width: item.width,
-fontFamily: item.fontFamily,
-fontWeight: item.fontWeight,
-fontSize: item.fontSize
-}}
-className={item.className} onClick={() => sortTable(order, item.sortable)}> {item.title}
-</div>
-)
-})
-}
-</div>
-</div>
-
-<div className='rowGrid'>
-<div className='tableData'>
-{filteredData.length ? renderTableData(filteredData) : renderTableData(sorting)}
-</div>
-</div>
-</div>
-</>
-)
+        <div className='rowGrid'>
+          <div className='tableData'>
+            {filteredData.length ? renderTableData(filteredData) : renderTableData(sorting)}
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Table;
--- venzo-data-grid/src/context/wholetable.js
+++ venzo-data-grid/src/context/wholetable.js
@@ -1,12 +1,12 @@
 const tableJson =
 {
-width: '65%',
-border: '1px solid white',
-margin: '5% 5%',
-backgroundColor: 'white',
-boxShadow: 'rgb(0 0 0 / 24%) 0px 3px 8px',
-padding:'20px 30px'
+    width: '65%',
+    border: '1px solid white',
+    margin: '5% 5%',
+    backgroundColor: 'white',
+    boxShadow: 'rgb(0 0 0 / 24%) 0px 3px 8px',
+    padding: '20px 30px'
 }
 
 
-export {tableJson}
+export { tableJson }
